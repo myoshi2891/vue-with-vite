4cb37911936a93a7d7cb8fd2b8276b80dd249743
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-23 01:14:16 +0900

    Little Confusing Things

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,7 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
@@ -47,14 +48,15 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Ecosystem</template>
 
-    Get official tools and libraries for your project:
+    Get official tools and libraries for your project: Now visit the 
     <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
     <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
     <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
     <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
     you need more resources, we suggest paying
     <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    a visit. <RouterLink to="/about">about</RouterLink>
+
   </WelcomeItem>
 
   <WelcomeItem>
