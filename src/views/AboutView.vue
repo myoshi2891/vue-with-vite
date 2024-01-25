58ab423fa93047cc5fbe039427db3080bfb72df1
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-25 23:47:35 +0900

    Direct Mutation Concerns

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {useFlash} from "@/composables/useFlash";
-
+import { counter } from "@/stores/counterStore";
 let { flash } = useFlash();
 </script>
 
@@ -8,6 +8,7 @@ let { flash } = useFlash();
   <div>
     <p>
       <button @click="flash('Yay', 'It works on the about page!', 'info')">Click Me</button>
+      About page. The current count is {{ counter.count }}
     </p>
   </div>
 </template>
